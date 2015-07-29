59923176e9e5d66bccc07538b40ca9c8b44d6409
andr7e <andr13e@gmail.com>
2015-07-29 13:27:56 +0300

    fix make boot.img for mtk

---
+++ bootimg.mk
@@ -0,0 +1,17 @@
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(recovery_ramdisk) $(recovery_kernel)
+
+	@echo ----- Making recovery image ------
+	cp $(MTK_TOOLS_DIR)/repack-MT65xx.pl repack-MT65xx.pl
+	cp $(MTK_TOOLS_DIR)/mkbootimg mkbootimg
+	# perl repack-MT65xx.pl -recovery $(recovery_kernel) $(RECOVERY_ROOT_DIR) $@
+	@echo ----- Made recovery image -------- $@
+	rm -rf repack-MT65xx.pl mkbootimg
+
+$(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_RAMDISK_TARGET) $(INSTALLED_KERNEL_TARGET) $(TARGET_ROOT_OUT)
+	
+	@echo ----- Making boot image ------
+	cp $(MTK_TOOLS_DIR)/repack-MT65xx.pl repack-MT65xx.pl
+	cp $(MTK_TOOLS_DIR)/mkbootimg mkbootimg
+	perl repack-MT65xx.pl -boot $(INSTALLED_KERNEL_TARGET) $(TARGET_ROOT_OUT) $@
+	@echo ----- Made boot image -------- $@
+	rm -rf repack-MT65xx.pl mkbootimg
