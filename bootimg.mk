1d5c0faf4caa28cc68fca7e8715f01b383bea1a4
andr7e <andr13e@gmail.com>
2015-08-01 16:37:28 +0300

    add recovery [fix building cm12.1]

--- bootimg.mk
+++ bootimg.mk
@@ -3,7 +3,7 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(recovery_ramdisk) $(recovery_kernel)
 	@echo ----- Making recovery image ------
 	cp $(MTK_TOOLS_DIR)/repack-MT65xx.pl repack-MT65xx.pl
 	cp $(MTK_TOOLS_DIR)/mkbootimg mkbootimg
-	# perl repack-MT65xx.pl -recovery $(recovery_kernel) $(RECOVERY_ROOT_DIR) $@
+	perl repack-MT65xx.pl -recovery $(recovery_kernel) $(PRODUCT_OUT)/recovery/root $@
 	@echo ----- Made recovery image -------- $@
 	rm -rf repack-MT65xx.pl mkbootimg
 
